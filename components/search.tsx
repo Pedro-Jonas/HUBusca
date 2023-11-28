1608dcc56763bb7118b5bcd83c720e85c10b5c4e
Pedro Jonas <pedrojonas.pj77@gmail.com>
2023-11-28 11:01:39 -0300

    feat: created search user

--- components/search.tsx
+++ components/search.tsx
@@ -1,12 +1,45 @@
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, Keyboard } from 'react-native';
+import React, {useState} from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import axios from 'axios'
 
 export default function Search() {
-    return (
-        <View style={styles.search}>
-            <TextInput style={styles.input} placeholder='Pesquisar' autoCapitalize='none' autoCorrect={false}/>
-        </View>
-    );
+  const [user, setUser] = useState(''); 
+  const [info, setInfo] = useState({})
+
+  const seachUser = async (user : string) => {
+    Keyboard.dismiss()
+    try {
+      const response = await axios.get(`https://api.github.com/users/${user}`)
+      setInfo({
+      name: response.data.name,
+      login: response.data.login,
+      avatar: response.data.avatar_url,
+      location: response.data.location
+      })
+      setUser('')
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  return (
+    <>
+      <View style={styles.search}>
+        <TextInput 
+        style = {styles.input} 
+        placeholder ='Pesquisar' 
+        autoCapitalize = 'none' 
+        autoCorrect = {false}
+        value = {user}
+        onChangeText={(value) => setUser(value)}
+        />
+        <Ionicons name='search' size={32} color='white' onPress={() => seachUser(user)} />
+      </View>
+    </>
+  );
 }
+
 
 const styles = StyleSheet.create({
   search: {
@@ -15,6 +48,7 @@ const styles = StyleSheet.create({
     width:'100%',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
   },
   input: {
     width: 200,
